52af94ffe3e48a1f038333fb3cee7bb55fd7c133
sharonjoji99 <nsjoji@ncsu.edu>
2023-11-10 00:06:23 -0500

    Add some parts

--- src/config/DatabaseConfig.java
+++ src/config/DatabaseConfig.java
@@ -2,8 +2,8 @@ package config;
 
 public class DatabaseConfig {
 
-    public static String UnityID = "nsjoji";
-    public static String SqlPassword= "123Sharonijoj123";
+    public static String UnityID = ""; // Enter your DB ID
+    public static String SqlPassword= ""; // Enter your password
 
     public static String jdbcURL = "jdbc:mariadb://classdb2.csc.ncsu.edu:3306/"+ UnityID;
 

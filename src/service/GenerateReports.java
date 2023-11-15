3e98a1d362c4442b9bf1f466e61aeecedfcf0610
amritav <amritavisalam@gmail.com>
2023-11-14 20:28:59 -0500

    Added extra condition to employee Permits in Reports wrt to Zones.

--- src/service/GenerateReports.java
+++ src/service/GenerateReports.java
@@ -175,13 +175,13 @@ public class GenerateReports {
                 System.out.println("Parking Lots in the database:");
                 resultSetService.runQueryAndPrintOutput2(conn, "SELECT * from ParkingLot;");
 
-                System.out.println("Zones in the database:");
-                resultSetService.runQueryAndPrintOutput2(conn, "SELECT * from Zone;");
+                System.out.println("Employee Zones in the database:");
+                resultSetService.runQueryAndPrintOutput2(conn, "SELECT * from Zone WHERE ZoneID IN ('A','B','C','D');");
 
                 System.out.println("Enter Parking Lot:");
                 final String parkingLot = scanner.nextLine();
 
-                System.out.println("Enter Zone ID (A,B,C,D,AS,BS,CS,DS,V):");
+                System.out.println("Enter Zone ID (A,B,C,D):");
                 final String zoneID = scanner.nextLine();
 
                 final String sqlQuery = "SELECT COUNT(D.DriverID) AS EmployessWithPermits FROM PermitLocation PL INNER JOIN Permit P ON PL.PermitID = P.PermitID INNER JOIN Vehicle V ON V.LicenseNo = P.LicenseNo  INNER JOIN Driver D ON V.DriverID = D.DriverID WHERE PL.PLName = ? AND PL.ZoneID = ? AND D.Status = 'E';";
@@ -193,10 +193,10 @@ public class GenerateReports {
                 resultSetService.viewFromResultSet(resultSet);
             }
             else if (option == 2) {
-                System.out.println("Zones in the database:");
-                resultSetService.runQueryAndPrintOutput2(conn, "SELECT * from Zone;");
+                System.out.println("Employee Zones in the database:");
+                resultSetService.runQueryAndPrintOutput2(conn, "SELECT * from Zone WHERE ZoneID IN ('A','B','C','D');");
 
-                System.out.println("Enter Zone ID (A,B,C,D,AS,BS,CS,DS,V):");
+                System.out.println("Enter Zone ID (A,B,C,D):");
                 final String zoneID = scanner.nextLine();
 
                 final String sqlQuery = "SELECT COUNT(D.DriverID) AS EmployessWithPermits FROM PermitLocation PL INNER JOIN Permit P ON PL.PermitID = P.PermitID INNER JOIN Vehicle V ON V.LicenseNo = P.LicenseNo  INNER JOIN Driver D ON V.DriverID = D.DriverID WHERE PL.ZoneID = ? AND D.Status = 'E';";
